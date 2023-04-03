c049f769913e05c2230b4fc0eea06a3037d75ad2
rajkumar7859 <rajkumarvishwakarma8759@gmail.com>
2023-04-03 13:41:32 +0530

    all done

--- Cointab_assign/frontend/src/Routes/Login.jsx
+++ Cointab_assign/frontend/src/Routes/Login.jsx
@@ -103,6 +103,7 @@ const Login = () => {
           className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded shadow shadow-xl focus:outline-none focus:shadow-outline"
           type="submit"
         >
+          
           {isLoading ? "Loading...." : "Login"}
         </button>
       </form>
